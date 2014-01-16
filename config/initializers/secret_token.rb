ace544c7e74d144be28b15d8631146660ee802d9
railsException@yahoo.com <cts_janardan@yaho.com>
2014-01-16 12:13:44 -0600

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyTemple::Application.config.secret_token = '048721fe20eb7f670cdb7079808b34be32701bc3b6552a864ac972ed023890fb0dc99c1d414498caf301848540ca96147dedf9180c49cfefa5c42e0a32b1ef47'
